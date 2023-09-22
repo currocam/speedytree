3e724064051a50c597563b1b556b80d4c88fa93b
Curro Campuzano <campuzanocurro@gmail.com>
2023-09-22 14:58:40 +0200

    refactor: using Options

--- src/rapid_nj/qmatrix.rs
+++ src/rapid_nj/qmatrix.rs
@@ -5,13 +5,12 @@ use crate::rapid_nj::node::Node;
 use std::collections::BTreeSet;
 
 struct QMatrix {
-    distances: Vec<Vec<f64>>,
-    sum_cols: Vec<f64>,
-    trees: Vec<BTreeSet<Node>>,
+    distances: Vec<Option<Vec<f64>>>,
+    sum_cols: Vec<Option<f64>>,
+    trees: Vec<Option<BTreeSet<Node>>>,
     u_max: f64,
     n: usize,
     n_leaves: usize,
-    merged_nodes: FixedBitSet,
 }
 
 impl QMatrix {
@@ -19,18 +18,18 @@ impl QMatrix {
         if i == j {
             0.0
         } else if i < j {
-            self.distances[i][j - i - 1]
+            self.distances[i].as_ref().unwrap()[j - i - 1]
         } else {
-            self.distances[j][i - j - 1]
+            self.distances[j].as_ref().unwrap()[i - j - 1]
         }
     }
-    pub fn distances_vec(distances: &Vec<Vec<f64>>, i: usize, j: usize) -> f64 {
+    pub fn distances_vec(distances: &Vec<Option<Vec<f64>>>, i: usize, j: usize) -> f64 {
         if i == j {
             0.0
         } else if i < j {
-            distances[i][j - i - 1]
+            distances[i].as_ref().unwrap()[j - i - 1]
         } else {
-            distances[j][i - j - 1]
+            distances[j].as_ref().unwrap()[i - j - 1]
         }
     }
 
@@ -38,14 +37,18 @@ impl QMatrix {
         let mut qmin = f64::INFINITY;
         let mut min_index = (0, 0);
         for (i, tree) in self.trees.iter().enumerate() {
+            if tree.is_none() {
+                continue;
+            }
+            let tree = tree.as_ref().unwrap();
             for node in tree.iter() {
                 let j = node.index;
-                if self.distance(i, j) - self.sum_cols[i] - self.u_max >= qmin {
+                if self.distance(i, j) - self.sum_cols[i].unwrap() - self.u_max >= qmin {
                     break;
                 }
                 let q = (self.n_leaves as f64 - 2.0) * self.distance(i, j)
-                    - self.sum_cols[i]
-                    - self.sum_cols[j];
+                    - self.sum_cols[i].unwrap()
+                    - self.sum_cols[j].unwrap();
                 if q < qmin {
                     qmin = q;
                     min_index = (i, j);
@@ -55,38 +58,38 @@ impl QMatrix {
         min_index
     }
     fn update(&mut self, i: usize, j: usize) {
-        self.trees[i].clear();
-        self.trees[j].clear();
-        self.merged_nodes.insert(i);
-        self.merged_nodes.insert(j);
-
+        self.trees[i] = None;
+        self.trees[j] = None;
+        self.sum_cols[i] = None;
+        self.sum_cols[j] = None;
         let distances = &mut self.distances;
-        self.sum_cols.push(0.0);
+        self.sum_cols.push(Some(0.0));
         for (m, row) in self.trees.iter_mut().enumerate() {
-            if self.merged_nodes.contains(m) {
+            if row.is_none() {
                 continue;
             }
+            let row = row.as_mut().unwrap();
 
             let dim = Self::distances_vec(distances, i, m);
             row.remove(&Node::new(i, dim));
-            self.sum_cols[m] -= dim;
-
             let djm = Self::distances_vec(distances, j, m);
             row.remove(&Node::new(j, djm));
-            self.sum_cols[m] -= djm;
 
             let new_distance = 0.5
                 * (Self::distances_vec(distances, i, m) + Self::distances_vec(distances, j, m)
                     - Self::distances_vec(distances, i, j));
             row.insert(Node::new(self.n, new_distance));
-            self.sum_cols[m] += new_distance;
-            self.sum_cols[self.n] += new_distance;
-            distances[m].push(new_distance);
+
+            self.sum_cols[m] = Some(self.sum_cols[m].unwrap() - dim - djm + new_distance);
+            self.sum_cols[self.n] = Some(self.sum_cols[self.n].unwrap() + new_distance);
+            distances[m].as_mut().unwrap().push(new_distance);
         }
         self.n_leaves -= 1;
         self.n += 1;
-        self.distances.push(Vec::with_capacity(self.n_leaves)); // Maybe add with capacity
-        self.trees.push(BTreeSet::new());
+        self.distances.push(Some(Vec::with_capacity(self.n_leaves))); // Maybe add with capacity
+        self.distances[i] = None;
+        self.distances[j] = None;
+        self.trees.push(Some(BTreeSet::new()));
     }
 }
 
@@ -97,10 +100,14 @@ impl From<&DistanceMatrix> for QMatrix {
         let n = d.size();
         let n_leaves = n;
         let matrix = &d.matrix;
-        let sum_cols: Vec<f64> = matrix.iter().map(|row| row.iter().sum::<f64>()).collect();
-        let u_max = *sum_cols
+        let sum_cols: Vec<Option<f64>> = matrix
             .iter()
-            .max_by(|a, b| a.partial_cmp(b).unwrap())
+            .map(|row| Some(row.iter().sum::<f64>()))
+            .collect();
+        let u_max = sum_cols
+            .iter()
+            .max_by(|a, b| a.unwrap().partial_cmp(&b.unwrap()).unwrap())
+            .unwrap()
             .unwrap();
         let mut distances = Vec::with_capacity(n);
         for i in 0..n {
@@ -108,24 +115,22 @@ impl From<&DistanceMatrix> for QMatrix {
             for j in i + 1..n {
                 row.push(matrix[i][j]);
             }
-            distances.push(row);
+            distances.push(Some(row));
         }
         let mut trees = Vec::with_capacity(n);
         for (row_index, row) in distances.iter().enumerate() {
             let mut tree = BTreeSet::new();
-            for (col_index, value) in row.iter().enumerate() {
+            for (col_index, value) in row.as_ref().unwrap().iter().enumerate() {
                 tree.insert(Node::new(col_index + row_index + 1, *value));
             }
-            trees.push(tree);
+            trees.push(Some(tree));
         }
-        let merged_nodes = FixedBitSet::with_capacity(2 * n - 1);
         QMatrix {
             distances,
             sum_cols,
             trees,
             u_max,
             n,
-            merged_nodes,
             n_leaves,
         }
     }
@@ -142,16 +147,19 @@ mod tests {
 
         let q = QMatrix::from(&d);
         // Check column sums
-        assert_eq!(q.sum_cols, vec![31.0, 34.0, 34.0, 30.0, 27.0]);
+        assert_eq!(
+            q.sum_cols,
+            vec![Some(31.0), Some(34.0), Some(34.0), Some(30.0), Some(27.0)]
+        );
         // Check only the upper triangle is stored
         assert_eq!(
             &q.distances,
             &vec![
-                vec![5.0, 9.0, 9.0, 8.0],
-                vec![10.0, 10.0, 9.0],
-                vec![8.0, 7.0],
-                vec![3.0],
-                vec![]
+                Some(vec![5.0, 9.0, 9.0, 8.0]),
+                Some(vec![10.0, 10.0, 9.0]),
+                Some(vec![8.0, 7.0]),
+                Some(vec![3.0]),
+                Some(vec![])
             ]
         );
         for i in 0..5 {
@@ -166,7 +174,7 @@ mod tests {
             Node::new(2, 9.0),
             Node::new(3, 9.0),
         ];
-        for (node, expected) in q.trees[0].iter().zip(expected_one.iter()) {
+        for (node, expected) in q.trees[0].as_ref().unwrap().iter().zip(expected_one.iter()) {
             assert_eq!(node, expected);
         }
     }
@@ -185,27 +193,30 @@ mod tests {
         assert_eq!(
             &q.distances,
             &vec![
-                vec![5.0, 9.0, 9.0, 8.0,],
-                vec![10.0, 10.0, 9.0],
-                vec![8.0, 7.0, 7.0],
-                vec![3.0, 7.0],
-                vec![6.0],
-                vec![]
+                None,
+                None,
+                Some(vec![8.0, 7.0, 7.0]),
+                Some(vec![3.0, 7.0]),
+                Some(vec![6.0]),
+                Some(vec![])
             ]
         );
-        assert_eq!(q.sum_cols, vec![31.0, 34.0, 22.0, 18.0, 16.0, 20.0]);
+        assert_eq!(
+            q.sum_cols,
+            vec![None, None, Some(22.0), Some(18.0), Some(16.0), Some(20.0)]
+        );
         assert_eq!(q.find_neighbors(), (3, 4));
         q.update(3, 4);
         assert_eq!(
             &q.distances,
             &vec![
-                vec![5.0, 9.0, 9.0, 8.0,],
-                vec![10.0, 10.0, 9.0],
-                vec![8.0, 7.0, 7.0, 6.0],
-                vec![3.0, 7.0],
-                vec![6.0],
-                vec![5.0],
-                vec![],
+                None,
+                None,
+                Some(vec![8.0, 7.0, 7.0, 6.0]),
+                None,
+                None,
+                Some(vec![5.0]),
+                Some(vec![])
             ]
         );
     }
