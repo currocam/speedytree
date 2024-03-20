62a13b2d246efe39c8884f7c619948da33c2f770
Curro Campuzano <campuzanocurro@gmail.com>
2024-03-20 19:21:36 +0100

    Split into binary and library

--- src/lib.rs
+++ src/lib.rs
@@ -20,10 +20,10 @@ pub mod property_tests;
 
 /// Configuration of the program
 pub mod configuration;
-mod distances;
-mod naive_nj;
-mod newick;
-mod rapid_nj;
+pub mod distances;
+pub mod naive_nj;
+pub mod newick;
+pub mod rapid_nj;
 use hybrid_nj::neighbor_joining;
 
 use crate::distances::DistanceMatrix;
@@ -39,46 +39,3 @@ use std::{
 type ResultBox<T> = std::result::Result<T, Box<dyn error::Error>>;
 type Tree = petgraph::graph::UnGraph<String, f64>;
 
-/// Available algorithms in the program
-#[derive(Debug, Clone)]
-pub enum Algorithm {
-    /// Naive neighbor joining
-    Naive,
-    /// Rapid neighbor joining
-    RapidNJ,
-    /// Hybrid neighbor joining
-    Hybrid,
-}
-/// Main function of the crate
-pub fn run(config: configuration::Config) {
-    rayon::ThreadPoolBuilder::new()
-        .num_threads(config.threads)
-        .build_global()
-        .unwrap();
-
-    let reader = io::stdin().lock();
-    let d = DistanceMatrix::build_from_phylip(reader).unwrap_or_else(|err| {
-        eprintln!("{err}");
-        process::exit(1);
-    });
-
-    let d = match config.algo {
-        Algorithm::Naive => naive_neighbor_joining(d),
-        Algorithm::RapidNJ => rapid_nj(d, config.chunk_size),
-        Algorithm::Hybrid => {
-            let naive_steps = d.size() * config.naive_percentage / 100;
-            dbg!(naive_steps);
-            neighbor_joining(d, naive_steps, config.chunk_size)
-        }
-    };
-    let graph = d.unwrap_or_else(|err| {
-        eprintln!("{err}");
-        process::exit(1);
-    });
-    io::stdout()
-        .write_all(to_newick(&graph).as_bytes())
-        .unwrap_or_else(|err| {
-            eprintln!("{err}");
-            process::exit(1);
-        });
-}
